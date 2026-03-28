09d3f10e5ffb0ab101d81f72bc3969209272e7d4
amorimlipeh <am.felipe1994@gmail.com>
2026-03-28 08:22:06 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,50 +1,14 @@
 
+require("dotenv").config();
 const express=require("express");
-const fs=require("fs");
-const path=require("path");
-
 const app=express();
-const PORT=process.env.PORT||3000;
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname,"public")));
 
-const dbPath=path.join(__dirname,"data/clientes.json");
+app.get("/",(req,res)=>res.send("RIO V5 PROFISSIONAL"));
 
-function load(){
- return JSON.parse(fs.readFileSync(dbPath));
-}
-function save(d){
- fs.writeFileSync(dbPath,JSON.stringify(d,null,2));
-}
+app.use("/api/auth",require("./server/routes/auth"));
+app.use("/api/clientes",require("./server/routes/clientes"));
 
-// auto bloqueio
-setInterval(()=>{
- let db=load();
- let now=Date.now();
- db.clientes.forEach(c=>{
-  if(now>c.vencimento && c.status!=="bloqueado"){
-   c.status="bloqueado";
-  }
- });
- save(db);
-},5000);
-
-// listar
-app.get("/api/clientes",(req,res)=>{
- res.json(load().clientes);
-});
-
-// pagar
-app.post("/api/pagar",(req,res)=>{
- let db=load();
- let c=db.clientes.find(x=>x.id==req.body.id);
- if(c){
-  c.vencimento=Date.now()+60000;
-  c.status="ativo";
-  save(db);
- }
- res.json({ok:true});
-});
-
-app.listen(PORT,()=>console.log("FINAL RODANDO"));
+const PORT=process.env.PORT||3000;
+app.listen(PORT,()=>console.log("V5 rodando "+PORT));
