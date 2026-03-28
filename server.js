183f10965d910c9f92f999b729f0ad78f127f6da
amorimlipeh <am.felipe1994@gmail.com>
2026-03-28 09:25:05 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -8,24 +8,16 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-// HOME agora carrega a interface real
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+// LOGIN
+app.get("/login", (req,res)=>{
+  res.sendFile(path.join(__dirname,"public/login.html"));
 });
 
-// AUTH
+// APP (SISTEMA REAL)
+app.get("/app", (req,res)=>{
+  res.sendFile(path.join(__dirname,"public/app.html"));
+});
+
 app.use("/api/auth", require("./server/routes/auth"));
 
-const { requireAuth } = require("./server/services/authService");
-
-// rota protegida de teste
-app.get("/api/auth-check", requireAuth, (req, res) => {
-  res.json({
-    ok: true,
-    usuario: req.user
-  });
-});
-
-app.listen(PORT, () => {
-  console.log("RIO FRONT + AUTH rodando na porta " + PORT);
-});
+app.listen(PORT, ()=>console.log("Fluxo profissional ativo " + PORT));
