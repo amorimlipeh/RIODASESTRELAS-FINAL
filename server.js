82b79ea1a86db64b17f0b1c13075d9d45e1ed4c4
amorimlipeh <am.felipe1994@gmail.com>
2026-03-28 08:48:03 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
-
 require("dotenv").config();
-const express=require("express");
-const app=express();
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get("/",(req,res)=>res.send("RIO V5 PROFISSIONAL"));
+app.get("/", (req, res) => res.send("RIO V5 PROFISSIONAL"));
 
-app.use("/api/auth",require("./server/routes/auth"));
-app.use("/api/clientes",require("./server/routes/clientes"));
+app.use("/api/auth", require("./server/routes/auth"));
+app.use("/api/clientes", require("./server/routes/clientes"));
 
-const PORT=process.env.PORT||3000;
-app.listen(PORT,()=>console.log("V5 rodando "+PORT));
+const { requireAuth } = require("./server/services/authService");
+
+// rota de teste protegida
+app.get("/api/auth-check", requireAuth, (req, res) => {
+  res.json({
+    ok: true,
+    usuario: req.user
+  });
+});
+
+app.listen(PORT, () => console.log("V5 rodando " + PORT));
