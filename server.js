0f49ac47e7bc16a9aa10fead8943a9be8bf2687b
amorimlipeh <am.felipe1994@gmail.com>
2026-03-27 22:32:44 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -8,13 +8,4 @@ const PORT=process.env.PORT||3000;
 app.use(express.json());
 app.use(express.static(path.join(__dirname,"public")));
 
-let rota=[
- {codigo:"A",endereco:"01-001-1-1",qtd:2},
- {codigo:"B",endereco:"01-010-1-1",qtd:1}
-];
-
-app.get("/api/picking",(req,res)=>{
- res.json({ok:true,rota});
-});
-
-app.listen(PORT,()=>console.log("VOZ ATIVA"));
+app.listen(PORT,()=>console.log("CAMERA ATIVA"));
