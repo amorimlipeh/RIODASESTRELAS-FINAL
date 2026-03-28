2d4803c7e8fbb9db900a0960cf0955fe8a99c324
amorimlipeh <am.felipe1994@gmail.com>
2026-03-28 08:58:38 -0300

    Add files via upload

--- server/routes/permissoes.js
+++ server/routes/permissoes.js
@@ -1,10 +1,10 @@
 
 const express = require("express");
 const router = express.Router();
+
 const { requireAuth } = require("../services/authService");
 const { requireRole } = require("../middleware/permissions");
 
-// só admin ou desenvolvedor
 router.get("/admin-only",
   requireAuth,
   requireRole(["admin","desenvolvedor"]),
