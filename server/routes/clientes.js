fedc970d91711c0dd92d5635a50803d24d423623
amorimlipeh <am.felipe1994@gmail.com>
2026-03-28 08:23:58 -0300

    Delete server/routes directory

--- server/routes/clientes.js
+++
@@ -1,9 +0,0 @@
-
-const express=require("express");
-const router=express.Router();
-
-router.get("/",(req,res)=>{
- res.json([{nome:"Cliente 1"},{nome:"Cliente 2"}]);
-});
-
-module.exports=router;
