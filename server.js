727ec6a8adf7d6cbd6609b73649d3a4f90b2eb99
amorimlipeh <am.felipe1994@gmail.com>
2026-03-27 22:38:03 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -8,4 +8,20 @@ const PORT=process.env.PORT||3000;
 app.use(express.json());
 app.use(express.static(path.join(__dirname,"public")));
 
-app.listen(PORT,()=>console.log("CAMERA ATIVA"));
+let pedidos=[
+ {id:1,itens:["A","B"],prioridade:2},
+ {id:2,itens:["C"],prioridade:1}
+];
+
+// IA decisão automática
+app.get("/api/auto",(req,res)=>{
+ let ordenado=[...pedidos].sort((a,b)=>a.prioridade-b.prioridade);
+
+ res.json({
+  ok:true,
+  proximo:ordenado[0],
+  fila:ordenado
+ });
+});
+
+app.listen(PORT,()=>console.log("AUTOMAÇÃO TOTAL"));
